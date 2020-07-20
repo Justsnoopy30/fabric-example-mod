cfce5c5dafb5e639155ae65dd945a6d0b15d6e4d
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-07-20 18:20:10 -0500

    Reproducable test case of the no obfuscation mapping error when using ECJ.

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,15 +1,17 @@
 package net.fabricmc.example.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(MinecraftServer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+	// A mixin to reproduce the problem
+	@Overwrite
+	private boolean shouldKeepTicking() {
+		// Just returning true to have something in the mixin
+		return true;
 	}
 }
